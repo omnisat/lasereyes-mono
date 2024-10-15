09fbc11c760d80ebfd75daacd4cc0e1974924603
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-10-15 15:34:10 -0700

    change build output directory for lasereyes-core

--- packages/lasereyes-core/src/constants/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './networks';
-export * from './wallets';
-export * from './settings';
-//# sourceMappingURL=index.d.ts.map
--- packages/lasereyes-core/src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export * from './networks';
-export * from './wallets';
-export * from './settings';
