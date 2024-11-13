f9e65fc15877f2aca287e9b5cab7cd52ba6b7eb7
hathbanger <hathbanger.ah@gmail.com>
2024-11-13 16:20:57 -0700

    err handle

--- apps/demo.lasereyes.build/components/App.tsx
+++ apps/demo.lasereyes.build/components/App.tsx
@@ -75,7 +75,7 @@ const App = ({ setNetwork }: { setNetwork: (n: NetworkType) => void }) => {
         switchNetwork(MAINNET)
         setNetwork(MAINNET)
       }
-    } catch (e) {
+    } catch (e: any) {
       toast.error(e.message)
     }
   }
