d773dd91502bab42b27548d6bf0a7a5526bb70e7
Ufedojo Atabo <ufedojoatabo@gmail.com>
2024-11-18 11:55:44 +0100

    fix vite build config
    Externalized the jsx transform library

--- packages/lasereyes/vite.config.ts
+++ packages/lasereyes/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       fileName: 'index',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
